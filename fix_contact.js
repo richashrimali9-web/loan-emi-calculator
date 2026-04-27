a33f8553654d7aa53f79ae5b849a9553e625e238
Richa Shrimali <richa.shrimali9@gmail.com>
2026-04-27 15:28:10 +0530

    deploy: sync public/ to root [2026-04-27]

--- fix_contact.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let c = fs.readFileSync('public/contact/index.html', 'utf8');
-
-c = c.replace(/placeholder="What is this about- \/>/, 'placeholder="What is this about?" />');
-
-fs.writeFileSync('public/contact/index.html', c);
